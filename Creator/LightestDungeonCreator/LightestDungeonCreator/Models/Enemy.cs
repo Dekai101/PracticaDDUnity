97fa00f15f33fd218490dbb72db64d302742e77a
Catalin <catalinst05@gmail.com>
2026-03-02 20:46:34 +0100

    Minor changes + rescaffold en creator

---
+++ Creator/LightestDungeonCreator/LightestDungeonCreator/Models/Enemy.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LightestDungeonCreator.Models;
+
+public partial class Enemy
+{
+    public int EntityId { get; set; }
+
+    public int PassiveId { get; set; }
+
+    public virtual Entity Entity { get; set; } = null!;
+
+    public virtual ICollection<Loottable> Loottables { get; set; } = new List<Loottable>();
+}
