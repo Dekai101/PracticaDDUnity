4f47454871321eb0041d782b4124feb2e9f039b4
Catalin <catalinst05@gmail.com>
2026-03-02 20:05:25 +0100

    Actualitzacions Splash Screen

---
+++ Creator/LightestDungeonCreator/LightestDungeonCreator/SplashScreen.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace LightestDungeonCreator
+{
+    /// <summary>
+    /// Lógica de interacción para SplashScreen.xaml
+    /// </summary>
+    public partial class SplashScreen : Window
+    {
+        public SplashScreen()
+        {
+            InitializeComponent();
+        }
+
+        private void ExitButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void CreateCharacter_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void CreateSkill_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
